4415c3cfa711f430740cbb2fc453aa0eb9e1fd84
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2018-07-04 16:18:06 +0100

    added version from Git Tag

--- opt/build.sh
+++ opt/build.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-echo "> Environment Variables"
-echo "GOROOT=${GOROOT}"
-echo "GOPATH=${GOPATH}"
-echo "GOOS=${GOOS}"
-echo "GOARCH=${GOARCH}"
-echo "BINARY=${BINARY}"
-echo "PROJECT_PATH=${PROJECT_PATH}"
-echo "GIT_HEAD=$(git rev-parse HEAD)"
+export GIT_VERSION=$(git tag)
 
-BIN_DIR=${GOPATH}/${PROJECT_PATH}/bin
-#SRC_DIR=${GOPATH}/${PROJECT_PATH}/src
+echo "> Environment Variables"
+echo "CGO_ENABLED=$CGO_ENABLED"
+echo "GOROOT=$GOROOT"
+echo "GOPATH=$GOPATH"
+echo "GOOS=$GOOS"
+echo "GOARCH=$GOARCH"
+echo "BINARY=$BINARY"
+echo "PROJECT_PATH=$PROJECT_PATH"
+echo "GIT_HEAD=$(git rev-parse HEAD)"
+echo "GIT_VERSION=$GIT_VERSION"
+
+BINARY=$BINARY-$GIT_VERSION
+
+echo "> Clean bin folder"
+rm ./bin/*
 
 echo "> Getting dependencies"
 go get ./src
 
 echo "> Building project"
-GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_DIR}/${BINARY} -v ./src
+CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build \
+    -ldflags "-X main.version=$GIT_VERSION" \
+    -o ./bin/$BINARY \
+    -v ./src
 
 echo "> Listing builds"
-ls -l ${BIN_DIR}
+ls -l $BIN_DIR
 
 echo "Done"
